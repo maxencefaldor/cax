aecb8d0631dab899ee52f0c3157467e99162a23b
Maxence Faldor <m.faldor22@imperial.ac.uk>
2024-09-16 10:49:09 +0000

    Fix indentation

--- cax/nn/pool.py
+++ cax/nn/pool.py
@@ -8,52 +8,52 @@ from flax import struct
 
 
 class Pool(struct.PyTreeNode):
-    """Pool class for for NCA training."""
+	"""Pool class for for NCA training."""
 
-    size: int = struct.field(pytree_node=False)
-    data: dict[str, jax.Array]
+	size: int = struct.field(pytree_node=False)
+	data: dict[str, jax.Array]
 
-    @classmethod
-    def create(cls, **kwargs):
-        """Create a new Pool instance from the given data.
+	@classmethod
+	def create(cls, **kwargs):
+		"""Create a new Pool instance from the given data.
 
-        Args:
-            **kwargs: Keyword arguments representing the data to be stored in the pool.
+		Args:
+			**kwargs: Keyword arguments representing the data to be stored in the pool.
 
-        Returns:
-            A new Pool instance.
+		Returns:
+			A new Pool instance.
 
-        """
-        size = next(iter(kwargs.values())).shape[0]
-        return cls(size=size, data=kwargs)
+		"""
+		size = next(iter(kwargs.values())).shape[0]
+		return cls(size=size, data=kwargs)
 
-    @partial(jax.jit, static_argnames=("sample_size",))
-    def sample(self, key: jax.Array, *, sample_size: int) -> tuple[jax.Array, dict[str, jax.Array]]:
-        """Sample a subset of data from the pool.
+	@partial(jax.jit, static_argnames=("sample_size",))
+	def sample(self, key: jax.Array, *, sample_size: int) -> tuple[jax.Array, dict[str, jax.Array]]:
+		"""Sample a subset of data from the pool.
 
-        Args:
-            key: A JAX random key.
-            sample_size: The number of items to sample.
+		Args:
+			key: A JAX random key.
+			sample_size: The number of items to sample.
 
-        Returns:
-            A tuple containing the sampled indices and the sampled data.
+		Returns:
+			A tuple containing the sampled indices and the sampled data.
 
-        """
-        index = jax.random.choice(key, self.size, shape=(sample_size,), replace=False)
-        sampled_data = {k: v[index] for k, v in self.data.items()}
-        return index, sampled_data
+		"""
+		index = jax.random.choice(key, self.size, shape=(sample_size,), replace=False)
+		sampled_data = {k: v[index] for k, v in self.data.items()}
+		return index, sampled_data
 
-    @jax.jit
-    def add(self, index: jax.Array, **kwargs) -> Self:
-        """Add items in the pool at the specified indices.
+	@jax.jit
+	def add(self, index: jax.Array, **kwargs) -> Self:
+		"""Add items in the pool at the specified indices.
 
-        Args:
-            index: The indices at which to add or update items.
-            **kwargs: The data to be added or updated.
+		Args:
+			index: The indices at which to add or update items.
+			**kwargs: The data to be added or updated.
 
-        Returns:
-            A new Pool instance with the updated data.
+		Returns:
+			A new Pool instance with the updated data.
 
-        """
-        updated_data = {k: self.data[k].at[index].set(kwargs[k]) for k in self.data}
-        return self.replace(data=updated_data)
+		"""
+		updated_data = {k: self.data[k].at[index].set(kwargs[k]) for k in self.data}
+		return self.replace(data=updated_data)
