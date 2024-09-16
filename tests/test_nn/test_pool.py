cd3b778de093bae7c92d124d5f065c00d374dc26
Maxence Faldor <m.faldor22@imperial.ac.uk>
2024-09-16 17:11:33 +0000

    Improve Pool implementation

--- tests/test_nn/test_pool.py
+++ tests/test_nn/test_pool.py
@@ -9,7 +9,7 @@ from cax.nn.pool import Pool
 @pytest.fixture
 def init_pool():
 	"""Create a Pool instance for testing."""
-	return Pool.create(a=jnp.array([1, 2, 3, 4, 5]), b=jnp.array([10, 20, 30, 40, 50]))
+	return Pool.create({"a": jnp.array([1, 2, 3, 4, 5]), "b": jnp.array([10, 20, 30, 40, 50])})
 
 
 def test_pool_create(init_pool):
@@ -24,7 +24,7 @@ def test_pool_create(init_pool):
 def test_pool_sample(init_pool):
 	"""Test the sample method of the Pool class."""
 	key = jax.random.PRNGKey(0)
-	index, sampled_data = init_pool.sample(key, sample_size=3)
+	index, sampled_data = init_pool.sample(key, batch_size=3)
 
 	assert index.shape == (3,)
 	assert "a" in sampled_data
@@ -33,13 +33,13 @@ def test_pool_sample(init_pool):
 	assert sampled_data["b"].shape == (3,)
 
 
-def test_pool_add(init_pool):
-	"""Test the add method of the Pool class."""
+def test_pool_update(init_pool):
+	"""Test the update method of the Pool class."""
 	new_index = jnp.array([0, 2, 4])
 	new_a = jnp.array([100, 200, 300])
 	new_b = jnp.array([1000, 2000, 3000])
 
-	updated_pool = init_pool.add(new_index, a=new_a, b=new_b)
+	updated_pool = init_pool.update(new_index, {"a": new_a, "b": new_b})
 
 	assert jnp.array_equal(updated_pool.data["a"], jnp.array([100, 2, 200, 4, 300]))
 	assert jnp.array_equal(updated_pool.data["b"], jnp.array([1000, 20, 2000, 40, 3000]))
