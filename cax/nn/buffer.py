c273b0142ccc5c51799cc8ec33ee8cf3e0f101db
Maxence Faldor <m.faldor22@imperial.ac.uk>
2024-09-17 12:15:43 +0000

    Add RL NCA notebook example

---
+++ cax/nn/buffer.py
@@ -0,0 +1,86 @@
+"""Buffer module."""
+
+from functools import partial
+from typing import Any, Self
+
+import jax
+import jax.numpy as jnp
+from flax import struct
+
+
+class Buffer(struct.PyTreeNode):
+	"""Buffer class."""
+
+	size: int = struct.field(pytree_node=False)
+	data: Any
+	is_full: jax.Array
+	index: jax.Array
+
+	@classmethod
+	def create(cls, size: int, datum: Any):
+		"""Create a new Buffer instance.
+
+		Args:
+			size: Size of the buffer.
+			datum: Unit of data used for inferring the structure of the experience data.
+
+		Returns:
+			A new Buffer instance.
+
+		"""
+		data = jax.tree.map(jnp.empty_like, datum)
+		data = jax.tree.map(lambda leaf: jnp.broadcast_to(leaf[None, ...], (size, *leaf.shape)), data)
+		return cls(
+			size=size,
+			data=data,
+			is_full=jnp.full((size,), False, dtype=jnp.bool),
+			index=jnp.array(0, dtype=jnp.int32),
+		)
+
+	@jax.jit
+	def add(self, batch: Any) -> Self:
+		"""Add a batch to the buffer.
+
+		Args:
+			batch: A batch to add to the buffer.
+
+		Returns:
+			A new Buffer instance with the added batch.
+
+		"""
+		batch_size = jax.tree.leaves(batch)[0].shape[0]
+		indices = self.index + jnp.arange(batch_size)
+		indices = indices % self.size
+
+		# Update data
+		data = jax.tree.map(
+			lambda data, batch: data.at[indices].set(batch),
+			self.data,
+			batch
+		)
+
+		# Update is_full and index
+		is_full = self.is_full.at[indices].set(True)
+		new_index = (self.index + batch_size) % self.size
+
+		return self.replace(
+			data=data,
+			is_full=is_full,
+			index=new_index
+		)
+
+	@partial(jax.jit, static_argnames=("batch_size",))
+	def sample(self, key: jax.Array, *, batch_size: int) -> tuple[jax.Array, Any]:
+		"""Sample a batch from the buffer.
+
+		Args:
+			key: A random key.
+			batch_size: The size of the batch to sample.
+
+		Returns:
+			A batch sampled from the buffer.
+
+		"""
+		indices = jax.random.choice(key, self.size, shape=(batch_size,), p=self.is_full)
+		batch = jax.tree.map(lambda leaf: leaf[indices], self.data)
+		return batch
