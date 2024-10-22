0b9393a56dfde1ed7463bd63c499af1d1fef71ba
Maxence Faldor <m.faldor22@imperial.ac.uk>
2024-10-22 14:26:07 +0000

    Wrap full state within nnx.Param

--- cax/core/update/elementary_update.py
+++ cax/core/update/elementary_update.py
@@ -2,6 +2,7 @@
 
 import jax
 import jax.numpy as jnp
+from flax import nnx
 from jax import Array
 
 from cax.core.update.update import Update
@@ -11,8 +12,8 @@ from cax.types import Input, Perception, State
 class ElementaryUpdate(Update):
 	"""Elementary Cellular Automata update class."""
 
-	patterns: Array
-	values: Array
+	patterns: nnx.Param
+	values: nnx.Param
 
 	def __init__(self, wolfram_code: str = "01101110"):
 		"""Initialize the ElementaryUpdate.
@@ -21,7 +22,7 @@ class ElementaryUpdate(Update):
 			wolfram_code: A string of 8 bits representing the Wolfram code for the automaton.
 
 		"""
-		self.patterns = jnp.array(
+		self.patterns = nnx.Param(jnp.array(
 			[
 				[1.0, 1.0, 1.0],
 				[1.0, 1.0, 0.0],
@@ -32,8 +33,8 @@ class ElementaryUpdate(Update):
 				[0.0, 0.0, 1.0],
 				[0.0, 0.0, 0.0],
 			]
-		)
-		self.values = jnp.array([int(bit) for bit in wolfram_code])
+		))
+		self.values = nnx.Param(jnp.array([int(bit) for bit in wolfram_code]))
 
 	def __call__(self, state: State, perception: Perception, input: Input | None = None) -> State:
 		"""Apply the Elementary Cellular Automata update rule.
@@ -51,5 +52,5 @@ class ElementaryUpdate(Update):
 		def update_pattern(pattern: Array, value: Array) -> Array:
 			return jnp.where(jnp.all(perception == pattern, axis=-1, keepdims=True), value, 0.0)
 
-		state = jnp.sum(jax.vmap(update_pattern)(self.patterns, self.values), axis=0)
+		state = jnp.sum(jax.vmap(update_pattern)(self.patterns.value, self.values.value), axis=0)
 		return state
